5d81639b34b0e9e5796d07da19354fa56a15bf79
Kittu Sabu <48944188+sabukittu@users.noreply.github.com>
2020-05-24 12:33:10 +0530

    Update centos_aws.sh

--- centos_aws.sh
+++ centos_aws.sh
@@ -18,59 +18,61 @@ exclude=kubelet kubeadm kubectl
 EOF
 fi
 
+ARG1=$1
+
 common () {
-## Basic Machine Setup
-	yum update -y && yum install vim wget -y
-	export LOCAL_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4 2>/dev/null)
-	setenforce 0
-	sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
-	echo -e "net.ipv4.ip_forward = 1 \nnet.bridge.bridge-nf-call-iptables = 1" >>/etc/sysctl.conf
-	if [[ $1 == 'master' ]]; then
-		echo -e "$LOCAL_IP k8-master" >>/etc/hosts
-		hostname k8-master
+  ## Basic Machine Setup
+  yum update -y && yum install git vim wget -y
+  export LOCAL_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4 2>/dev/null)
+  setenforce 0
+  sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+  echo -e "net.ipv4.ip_forward = 1 \nnet.bridge.bridge-nf-call-iptables = 1" >>/etc/sysctl.conf
+  if [[ $ARG1 == 'master' ]]; then
+    echo -e "$LOCAL_IP k8-master" >>/etc/hosts
+    hostname k8-master
   else 
-  	echo -e "$LOCAL_IP k8-node" >>/etc/hosts
-		hostname k8-node
-	fi
+    echo -e "$LOCAL_IP k8-node" >>/etc/hosts
+    hostname k8-node
+  fi
 
-## Docker Installation 
-	curl -fsSL https://get.docker.com | bash
-	curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-	chmod +x /usr/local/bin/docker-compose
-	systemctl start docker
-	systemctl enable docker
-	usermod -aG docker centos
+  ## Docker Installation 
+  curl -fsSL https://get.docker.com | bash
+  curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+  systemctl start docker
+  systemctl enable docker
+  usermod -aG docker centos
 
-## Kubernetes Installation
-	yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
-	systemctl enable --now kubelet
-	sysctl -p
+  ## Kubernetes Installation
+  yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+  systemctl enable --now kubelet
+  sysctl -p
 }
 
 master () {
-	export KUBECONFIG=/etc/kubernetes/admin.conf
-	kubeadm init --apiserver-advertise-address=$LOCAL_IP --pod-network-cidr=192.168.0.0/16 --ignore-preflight-errors=NumCPU && sleep 30
-	kubectl apply -f https://docs.projectcalico.org/v3.11/manifests/calico.yaml
-	mkdir -p $HOME/.kube \
-		 && cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  export KUBECONFIG=/etc/kubernetes/admin.conf
+  kubeadm init --apiserver-advertise-address=$LOCAL_IP --pod-network-cidr=192.168.0.0/16 --ignore-preflight-errors=NumCPU && sleep 30
+  kubectl apply -f https://docs.projectcalico.org/v3.11/manifests/calico.yaml
+  mkdir -p $HOME/.kube \
+    && cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 }
 
 case $1 in
-	'master')
-		common
-		master
-		;;
-	'node')
-		common
-		;;
-	'master-init')
-	  master
-	  ;;
-	'join-cmd')
-		kubeadm token create --print-join-command
-		;;
-	*)
-		echo "$0 <'master' or 'node'>"
-		exit 1
-		;;
+  'master')
+    common
+    master
+    ;;
+  'node')
+    common
+    ;;
+  'master-init')
+    master
+    ;;
+  'join-cmd')
+    kubeadm token create --print-join-command
+    ;;
+  *)
+    echo "$0 <'master' or 'node'>"
+    exit 1
+    ;;
 esac
