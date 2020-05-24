8433c5cd05dddd4485a8b059954bfd35c3ceaf64
Kittu Sabu <sabukittu@gmail.com>
2020-05-24 23:09:13 +0530

    update

--- centos_aws.sh
+++ centos_aws.sh
@@ -19,11 +19,11 @@ EOF
 fi
 
 ARG1=$1
+export LOCAL_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4 2>/dev/null)
 
 common () {
   ## Basic Machine Setup
   yum update -y && yum install git vim wget -y
-  export LOCAL_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4 2>/dev/null)
   setenforce 0
   sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
   echo -e "net.ipv4.ip_forward = 1 \nnet.bridge.bridge-nf-call-iptables = 1" >>/etc/sysctl.conf
@@ -54,10 +54,21 @@ master () {
   kubeadm init --apiserver-advertise-address=$LOCAL_IP --pod-network-cidr=192.168.0.0/16 --ignore-preflight-errors=NumCPU && sleep 30
   kubectl apply -f https://docs.projectcalico.org/v3.11/manifests/calico.yaml
   mkdir -p $HOME/.kube \
-    && cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+    && ln -s /etc/kubernetes/admin.conf $HOME/.kube/config
 }
 
-case $1 in
+reinit () {
+  kubeadm reset --force
+  rm -rf $HOME/.kube/config
+  if [[ $ARG1 == 'master-reinit' ]]; then
+    hostname k8-master
+    master
+  elif [[ $ARG1 == 'node-reinit' ]]; then
+    hostname k8-node 
+  fi
+}
+
+case $ARG1 in
   'master')
     common
     master
@@ -71,8 +82,74 @@ case $1 in
   'join-cmd')
     kubeadm token create --print-join-command
     ;;
+  'master-reinit')
+    reinit
+    ;;
+  'node-reinit')
+    reinit
+    ;;
   *)
-    echo "$0 <'master' or 'node'>"
+    echo "$0 <'master' or 'node' or 'master-init' or 'join-cmd' or 'master-reinit' or 'node-reinit'>"
     exit 1
     ;;
 esac
+
+
+### AWS UserData for Master and Worker 
+
+## Master
+
+# Content-Type: multipart/mixed; boundary="//"
+# MIME-Version: 1.0
+
+# --//
+# Content-Type: text/cloud-config; charset="us-ascii"
+# MIME-Version: 1.0
+# Content-Transfer-Encoding: 7bit
+# Content-Disposition: attachment; filename="cloud-config.txt"
+
+# #cloud-config
+# cloud_final_modules:
+# - [scripts-user, always]
+
+# --//
+# Content-Type: text/x-shellscript; charset="us-ascii"
+# MIME-Version: 1.0
+# Content-Transfer-Encoding: 7bit
+# Content-Disposition: attachment; filename="userdata.txt"
+
+# #!/bin/bash
+# export KUBECONFIG=/etc/kubernetes/admin.conf
+# export LOCAL_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4 2>/dev/null)
+# hostname k8-master
+# kubeadm reset --force
+# kubeadm init --apiserver-advertise-address=$LOCAL_IP --pod-network-cidr=192.168.0.0/16 --ignore-preflight-errors=NumCPU | egrep -i -A 1 "kubeadm join $LOCAL_IP" >/tmp/k8snode-join.log
+# kubectl apply -f https://docs.projectcalico.org/v3.11/manifests/calico.yaml
+# --//
+
+
+## Worker
+
+# Content-Type: multipart/mixed; boundary="//"
+# MIME-Version: 1.0
+
+# --//
+# Content-Type: text/cloud-config; charset="us-ascii"
+# MIME-Version: 1.0
+# Content-Transfer-Encoding: 7bit
+# Content-Disposition: attachment; filename="cloud-config.txt"
+
+# #cloud-config
+# cloud_final_modules:
+# - [scripts-user, always]
+
+# --//
+# Content-Type: text/x-shellscript; charset="us-ascii"
+# MIME-Version: 1.0
+# Content-Transfer-Encoding: 7bit
+# Content-Disposition: attachment; filename="userdata.txt"
+
+# #!/bin/bash
+# hostname k8-node
+# kubeadm reset --force
+# --//
